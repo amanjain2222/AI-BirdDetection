ce0328d9134547410691895e20428bf461db91be
Aman jain <ajai0035@student.monash.edu>
2025-05-15 19:25:34 +1000

    added some comments

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 resource "aws_s3_bucket" "BirdStoreBucket" {
-  bucket = "fit5225-birdstore"
+  bucket = "fit5225-birdstore" # name should be globally unique so change the name when you run this
 
   tags = {
     Name        = "BirdStore"
