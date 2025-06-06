ce77d4123280a430efef788844ae9521cc5a5534
Xi <xxuu0117@student.monash.edu>
2025-06-06 13:07:02 +1000

    Latest version of data models.py

--- Query-by-tags-Xi/models.py
+++ Query-by-tags-Xi/models.py
@@ -1,24 +1,43 @@
 from pynamodb.models import Model
 from pynamodb.attributes import UnicodeAttribute, NumberAttribute, MapAttribute
 
-class FileModel(Model):
+class BirdBaseModel(Model):
     class Meta:
-        table_name = "UploadedFiles"
+        table_name = "BirdBase"
         region = 'us-east-1'
         read_capacity_units = 1
         write_capacity_units = 1
 
     # Primary key
-    FileId = UnicodeAttribute(hash_key=True)
+    BirdID = UnicodeAttribute(hash_key=True)
     
     # File type: image, video or audio
     FileType = UnicodeAttribute()
 
     # Link to the file in S3
-    S3URL = UnicodeAttribute()
+    MediaURL = UnicodeAttribute()
 
     # Link to the image thumbnail
     ThumbnailURL = UnicodeAttribute(null=True)
 
-    # Tags of the file
-    FileTags = MapAttribute(of=NumberAttribute, null=True)
+    # Uploaded date
+    UploadedDate = UnicodeAttribute()
+
+    # Uploader's username
+    Uploader = UnicodeAttribute()
+
+class BirdBaseIndexModel(Model):
+    class Meta:
+        table_name = "BirdBaseIndex"
+        region = 'us-east-1'
+        read_capacity_units = 1
+        write_capacity_units = 1
+
+    # Primary key
+    TagName = UnicodeAttribute(hash_key=True)
+
+    # Number of a specific species
+    TagValue = NumberAttribute()
+
+    # UUID of the media file
+    BirdID = UnicodeAttribute(range_key=True)
